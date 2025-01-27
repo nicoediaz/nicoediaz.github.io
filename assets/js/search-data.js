5b939cd0559ae7ab2bfe64da9d26c7cabe7676e9
nicoediaz <nicoediaz@gmail.com>
2025-01-27 21:45:36 +0000

    Deploying to gh-pages from @ nicoediaz/nicoediaz.github.io@b47eb517fe3af8cafd72e50bbae858a4f1343360 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,8 +23,8 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/teaching/";
           },
-        },{id: "nav-cv",
-          title: "cv",
+        },{id: "nav-false",
+          title: "false",
           description: "This is a description of the page. You can modify it in &#39;_pages/cv.md&#39;. You can also change or remove the top pdf download button.",
           section: "Navigation",
           handler: () => {
